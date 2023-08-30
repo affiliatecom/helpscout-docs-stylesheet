ce96472d1ed41598007b247c8c9bb2b6ef5bd66c
Eric Busch <ericbusch@gmail.com>
2023-08-30 14:53:52 -0400

    Update custom-scripts.js

--- custom-scripts.js
+++ custom-scripts.js
@@ -39,7 +39,7 @@
     function convertImagesToLightboxLinks() {
         $("img").each(function () {
             let src = $(this).attr('src');
-            $(this).wrapInner('<a href="' + src + '" target="_blank" class="img-link"></a>');
+            $(this).wrap('<a href="' + src + '" target="_blank" class="img-link"></a>');
         });
     }
 
